f574a6bdc21e19f385bd2b1466a0e3af2e304139
wendelleloi <wendelleloi2@gmail.com>
2021-05-09 19:23:14 -0300

    add redux-devtools-extension into store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,16 @@
 import { createStore } from 'redux';
-import { ICartState } from './modules/cart/types';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+import { ICartState } from './modules/cart/types';
 import rootReducer from './modules/rootReducer'
 
 export interface IState {
   cart: ICartState
 }
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools()
+);
 
 export default store;
